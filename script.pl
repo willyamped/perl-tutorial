975574429cf4ab1201f92a9600f0410cb0aeab82
Willy Angga <ampedwilly@gmail.com>
2024-06-06 16:07:56 +0700

    Explore better try catch

--- script.pl
+++ script.pl
@@ -5,9 +5,27 @@ use warnings;
 use feature qw/ say /;
 
 use Data::Dumper;
-use Dog;
-use OurModule;
+use Try::Tiny::SmartCatch;
+use Exception::Class(
+  'MyException',
+  'App::Filesystem::Full'
+);
 
-say OurModule::add_nums( 3, 2);
+try sub {
+  if (rand(5) > 2) {
+    App::Filesystem::Full->throw(error => 'oh no, the disk space is full!');
+  } else {
+    MyException->throw(error => 'something is not right')
+  }
+},
+catch_when 'MyException' => sub {
+  say "My exception!";
+},
+catch_when 'App::Filesystem::Full' => sub {
+  say "I know this exception!: $_"
+},
+catch_default sub {
+  say "$_";
+};
 
 1;
