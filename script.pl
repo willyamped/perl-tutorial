3a9118b16c27cd270d4833a16829106dcf3a2354
Willy Angga <ampedwilly@gmail.com>
2024-06-06 16:19:45 +0700

    Make web request

--- script.pl
+++ script.pl
@@ -5,27 +5,28 @@ use warnings;
 use feature qw/ say /;
 
 use Data::Dumper;
-use Try::Tiny::SmartCatch;
-use Exception::Class(
-  'MyException',
-  'App::Filesystem::Full'
-);
+use Mojo::UserAgent;
+use Mojo::Promise;
 
-try sub {
-  if (rand(5) > 2) {
-    App::Filesystem::Full->throw(error => 'oh no, the disk space is full!');
-  } else {
-    MyException->throw(error => 'something is not right')
-  }
-},
-catch_when 'MyException' => sub {
-  say "My exception!";
-},
-catch_when 'App::Filesystem::Full' => sub {
-  say "I know this exception!: $_"
-},
-catch_default sub {
-  say "$_";
-};
+my $post = 'https://jsonplaceholder.typicode.com/posts/1';
 
+my $UA = Mojo::UserAgent->new;
+
+# my $Result = $UA->get($post)->result;
+# say $Result->is_success;
+# say $Result->code;
+# say $Result->body;
+
+my @promises = map {
+  $UA->get_p('https://jsonplaceholder.typicode.com/posts/1')
+} 1 .. 10;
+
+Mojo::Promise->all(@promises)
+  ->then(
+    sub {
+      my @resolved = @_;
+      say Dumper($_->[0]->result->json) for @resolved;
+    }
+  )
+  ->wait;
 1;
